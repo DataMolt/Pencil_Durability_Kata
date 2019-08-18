92f539800e9815e07d05bf3208c87180b7d2d8f4
Adam <lottadam@umich.edu>
2019-08-18 15:45:44 -0400

    Added test for lower case reduction rate

--- Pencil_Durability_Unit_Tests/UnitTest1.cs
+++ Pencil_Durability_Unit_Tests/UnitTest1.cs
@@ -6,6 +6,20 @@ namespace Pencil_Durability_Unit_Tests
 {
     public class UnitTest1
     {
-        
+        [Theory]
+        [InlineData('a')]
+        [InlineData('b')]
+        [InlineData('.')]
+        [InlineData(',')]
+        [InlineData('!')]
+        public void LowercaseLettersReturnReductionRateOfOne(char letter)
+        {
+            // act
+            var sut = new Pencil();
+            var num = sut.FindCharReductionRate(letter);
+
+            // assert
+            Assert.Equal(1, num);
+        }
     }
 }
